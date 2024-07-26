03f3eb091c5d7261cf8f26ee5d6765dc4b4d5af5
yuichi04 <crgone.910428.en@gmail.com>
2024-07-27 00:37:43 +0900

    データベースから記事データを取得する処理を追加

--- database/main.go
+++ database/main.go
@@ -3,26 +3,71 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"go-myapi/database/models"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// 接続に使うユーザー・パスワード・データベース名を定義
 	dbUser := "docker"
 	dbPassword := "docker"
 	dbDatabase := "sampledb"
+
+	// データベースに接続するためのアドレス文を定義
+	// ここでは"docker:docker@tcp(127.0.0.1"3306)/sampledb?parseTime=true"となる
 	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser,
 		dbPassword, dbDatabase)
 
+	// Open関数を用いてデータベースに接続
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		fmt.Println(err)
 	}
+	// プログラムが終了するとき、コネクションが close されるようにする
 	defer db.Close()
 
-	if err := db.Ping(); err != nil {
+	// // sql.DB 型の Ping メソッドで疎通確認をする
+	// if err := db.Ping(); err != nil {
+	// 	// 失敗したらエラーを出力
+	// 	fmt.Println(err)
+	// } else {
+	// 	// 成功したらメッセージを出力
+	// 	fmt.Println("connect to DB")
+	// }
+
+	const sqlStr = `
+		select * from articles;
+	`
+
+	rows, err := db.Query(sqlStr)
+	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("connect to DB")
+		return
 	}
+	defer rows.Close() // Rows型もHTTPリクエストボディ同様「使い終わったらCloseしなければならない」類の構造体
+
+	articleArray := make([]models.Article, 0)
+
+	// rows に存在するレコードそれぞれに対して、繰り返し処理を実行する
+	for rows.Next() {
+		// 変数 article の各フィールドに、取得レコードのデータを入れる
+		// （SQL クエリの select 句から、タイトル・本文・ユーザー名・いいね数が返ってくることはわかっている）
+		var article models.Article
+		var createdTime sql.NullTime
+		err := rows.Scan(&article.ID, &article.Title, &article.Contents,
+			&article.UserName, &article.NiceNum, &createdTime)
+
+		if createdTime.Valid {
+			article.CreatedAt = createdTime.Time
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			articleArray = append(articleArray, article)
+		}
+	}
+
+	fmt.Printf("%+v\n", articleArray)
 }
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.22.4
 
 require github.com/labstack/echo/v4 v4.12.0
 
+require filippo.io/edwards25519 v1.1.0 // indirect
+
 require (
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
