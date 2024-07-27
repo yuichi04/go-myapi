3be9217b82d140748ca6c556465814f12ff6f71e
yuichi04 <crgone.910428.en@gmail.com>
2024-07-27 23:16:37 +0900

    3-8完了

---
+++ repositories/articles.go
@@ -0,0 +1,130 @@
+package repositories
+
+import (
+	"database/sql"
+	"go-myapi/database/models"
+)
+
+const (
+	articleNumPerPage = 5
+)
+
+// 新規投稿をデータベースにinsertする関数
+// -> データベースに保存した記事内容と、発生したエラーを返り値にする
+func InsertArticle(db *sql.DB, article models.Article) (models.Article, error) {
+	const sqlStr = `
+		insert into articles (title, contents, username, nice, created_at) values (?, ?, ?, 0, now());
+	`
+
+	var newArticle models.Article
+	newArticle.Title = article.Title
+	newArticle.Contents = article.Contents
+	newArticle.UserName = article.UserName
+
+	// 構造体`models.Article`を受け取って、それをデータベースに挿入する処理
+	res, err := db.Exec(sqlStr, article.Title, article.Contents, article.UserName, article.NiceNum)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	id, _ := res.LastInsertId()
+
+	newArticle.ID = int(id)
+
+	return newArticle, nil
+}
+
+// 変数 page で指定されたページに表示する投稿一覧をデータベースから取得する関数
+// -> 取得した記事データと、発生したエラーを返り値にする
+func SelectArticleList(db *sql.DB, page int) ([]models.Article, error) {
+	const sqlStr = `
+		select article_id, title, contents, username, nice
+		from articles
+		limit ? offset ?;
+	`
+
+	// 指定された記事データをデータベースから取得して、
+	// それを models.Article構造体のスライス[]models.Articleに詰めて返す処理
+	rows, err := db.Query(sqlStr, articleNumPerPage, ((page - 1) * articleNumPerPage))
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	articleArray := make([]models.Article, 0)
+	for rows.Next() {
+		var article models.Article
+		rows.Scan(&article.ID, &article.Title, &article.Contents, &article.UserName, &article.NiceNum)
+
+		articleArray = append(articleArray, article)
+	}
+
+	return articleArray, nil
+}
+
+// 記事 ID を指定して、記事データを取得する関数
+// -> 取得した記事データと、発生したエラーを返り値にする
+func SelectArticleDetail(db *sql.DB, articleID int) (models.Article, error) {
+	const sqlStr = `
+		select *
+		from articles
+		where article_id = ?;
+	`
+
+	// 指定IDの記事データをデータベースから取得して、それをmodels.Articleの構造体の形で返す処理
+	row := db.QueryRow(sqlStr, articleID)
+	if err := row.Err(); err != nil {
+		return models.Article{}, err
+	}
+
+	var article models.Article
+	var createdTime sql.NullTime
+	err := row.Scan(&article.ID, &article.Title, &article.Contents,
+		&article.UserName, &article.NiceNum, &createdTime)
+	if err != nil {
+		return models.Article{}, err
+	}
+
+	if createdTime.Valid {
+		article.CreatedAt = createdTime.Time
+	}
+
+	return article, nil
+}
+
+// いいね数をupdateする関数
+// -> 発生したエラーを返り値にする
+func UpdateNiceNum(db *sql.DB, articleID int) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	const sqlGetNice = `select nice from articles where article_id = ?;`
+
+	row := tx.QueryRow(sqlGetNice, articleID)
+	if err := row.Err(); err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	var nicenum int
+	err = row.Scan(&nicenum)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	const sqlUpdateNice = `update articles set nice = ? where article_id = ?;`
+	_, err = tx.Exec(sqlUpdateNice, nicenum+1, articleID)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Commit(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ repositories/comments.go
@@ -0,0 +1,51 @@
+package repositories
+
+import (
+	"database/sql"
+	"go-myapi/database/models"
+)
+
+// 新規投稿をデータベースにinsertする関数
+// ->データベースに保存したコメント内容と、発生したエラーを戻り値にする
+func InsertComment(db *sql.DB, comment models.Comment) (models.Comment, error) {
+	const sqlStr = `insert into comments (article_id, message, created_at) values (?, ?, now());`
+
+	var newComment models.Comment
+	newComment.ArticleID = comment.ArticleID
+	newComment.Message = comment.Message
+
+	result, err := db.Exec(sqlStr, comment.ArticleID, comment.Message)
+	if err != nil {
+		return models.Comment{}, err
+	}
+	id, _ := result.LastInsertId()
+	newComment.CommentID = int(id)
+
+	return newComment, nil
+}
+
+// 指定IDの記事についたコメント一覧を取得する関数
+// -> 取得したコメントデータと、発生したエラーを戻り値にする
+func SelectCommentList(db *sql.DB, articleID int) ([]models.Comment, error) {
+	const sqlStr = `select * from comments where article_id = ?;`
+
+	rows, err := db.Query(sqlStr, articleID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	commentArray := make([]models.Comment, 0)
+	for rows.Next() {
+		var comment models.Comment
+		var createdTime sql.NullTime
+		rows.Scan(&comment.CommentID, &comment.ArticleID, &comment.Message, &createdTime)
+
+		if createdTime.Valid {
+			comment.CreatedAt = createdTime.Time
+		}
+
+		commentArray = append(commentArray, comment)
+	}
+	return commentArray, nil
+}
