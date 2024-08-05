6e39ebd9ee317edddb1a4c44f97bbab2102bb631
yuichi04 <crgone.910428.en@gmail.com>
2024-08-05 23:13:33 +0900

    ハンドラ層をコントローラ層に変更

---
+++ controllers/controllers.go
@@ -0,0 +1,121 @@
+package controllers
+
+import (
+	"encoding/json"
+	"go-myapi/models"
+	"go-myapi/services"
+	"io"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type MyAppController struct {
+	service *services.MyAppService
+}
+
+func NewMyAppController(s *services.MyAppService) *MyAppController {
+	return &MyAppController{service: s}
+}
+
+// GET /hello のハンドラ
+func HelloHandler(w http.ResponseWriter, req *http.Request) {
+	// GETメソッド以外は許可しない
+	if req.Method != http.MethodGet {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		return
+	}
+	// GETメソッドのリクエストに対してレスポンスを返す
+	io.WriteString(w, "Hello, World!\n")
+}
+
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArtcile models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArtcile); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+	}
+	article, err := c.service.PostArticleService(reqArtcile)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func (c *MyAppController) ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+	queryMap := req.URL.Query()
+	var page int
+	if p, ok := queryMap["page"]; ok && len(p) > 0 {
+		var err error
+		page, err = strconv.Atoi(p[0])
+		if err != nil {
+			http.Error(w, "invalid query parameter", http.StatusBadRequest)
+			return
+		}
+	} else {
+		page = 1
+	}
+
+	articleList, err := c.service.GetArticleListService(page)
+	if err != nil {
+		http.Error(w, "invalid query paramter", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(articleList)
+}
+
+func (c *MyAppController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+	parts := strings.Split(req.URL.Path, "/")
+	if len(parts) < 3 {
+		http.Error(w, "invalid query parameter", http.StatusBadRequest)
+		return
+	}
+
+	articleID, err := strconv.Atoi(parts[2])
+	if err != nil {
+		http.Error(w, "invalid query parameter", http.StatusBadRequest)
+		return
+	}
+
+	article, err := c.service.GetArticleService(articleID)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(article)
+}
+
+func (c *MyAppController) PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+	var reqArticle models.Article
+	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	updatedArticle, err := c.service.PostNiceService(reqArticle)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(updatedArticle)
+}
+
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+	var reqComment models.Comment
+	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
+		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
+		return
+	}
+
+	newComment, err := c.service.PostCommentService(reqComment)
+	if err != nil {
+		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
+		return
+	}
+
+	json.NewEncoder(w).Encode(newComment)
+}
