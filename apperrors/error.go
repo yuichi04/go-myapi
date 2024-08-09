7c8f716842ca6b39ecf14e92d0925a6a23cd4132
yuichi04 <crgone.910428.en@gmail.com>
2024-08-10 01:32:13 +0900

    独自エラー型を作成

---
+++ apperrors/error.go
@@ -0,0 +1,18 @@
+package apperrors
+
+type MyAppError struct {
+	ErrCode        // レスポンスとログに表示するエラーコード
+	Message string // レスポンスに表示するエラーメッセージ
+	Err     error  // エラーチェーンのための内部エラー
+}
+
+// Errorメソッドは、その構造体をエラーとして扱うためにMUSTでつけるメソッド
+// 本体のErrorメソッドの役割は、
+// 「そのエラーがfmt.Print系関数等で出力されるときにどのような文字列になるか」ということを決めるためのもの
+func (myErr *MyAppError) Error() string {
+	return myErr.Err.Error()
+}
+
+func (myErr *MyAppError) Unwrap() error {
+	return myErr.Err
+}
